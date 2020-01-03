ec8c425f5b77c97c56b72291cb7cc9d99e11a3ca
Piotr Skorupa <pr_p.skorupka@neurosys.pl>
2020-01-03 08:41:10 +0100

    fully working POST method

--- http_example.cpp
+++ http_example.cpp
@@ -1,26 +1,29 @@
 #include <iostream>
+
 #include "signomix.hpp"
 
 int main()
 {
     std::cout << "Sending HTTP request POST" << std::endl;
 
-    signomix::HttpClient http;
-    http.setUrl("https://signomix.com/api/i4t");
-    http.setEui("0000-0000-0000");
-    http.setSecret("34d97xxx00112");
+    signomix::HttpClient http("0000-0000-0000", "34d97xxx00112");
     http.addField("led_green", 1);
     http.addField("led_red", 0.0);
     http.addField("led_yellow", 1.0);
-    http.setMethod(signomix::Method::POST);
 
-    auto response = http.send();
+    auto response = http.sendPost();
 
     std::cout << "Response curl code: " << response.curlCode << std::endl;
     std::cout << "Response curl code description: " << response.description << std::endl;
     std::cout << "Response HTTP code: " << response.httpCode << std::endl;
+    std::cout << "Response data: ";
 
-    std::cout << response.data.memory.size() << std::endl;
+    if (not response.data.empty())
+    {
+        std::copy(response.data.begin(), response.data.end(), std::ostreambuf_iterator<char>(std::cout));
+        std::cout << " [size = " << response.data.size() << "]";
+    }
+    std::cout << std::endl;
 
     if(response.error)
     {
--- signomix.hpp
+++ signomix.hpp
@@ -11,32 +11,27 @@ constexpr auto __DEFALUT_CODE = 0;
 constexpr auto HTTP_CREATED = 201;
 constexpr auto CURL_NO_ERROR = "No error";
 
-struct ByteData
-{
-  std::vector<char> memory;
-};
+using ByteData = std::vector<char>;
 
-size_t writeCallback(void *buffer, size_t len, size_t nmemb, void *userp)
+size_t writeCallback(void *contents, size_t size, size_t nmemb, void *userp)
 {
-    std::cout << "Callback run!" << std::endl;
-
-    if (not buffer or not userp)
+    size_t realsize = size * nmemb;
+    if (not contents or not userp)
     {
-        std::cerr << "No data in response" << std::endl;
+        std::cerr << "No data in response\n";
     }
 
     ByteData* data = static_cast<ByteData*>(userp);
-    char* buff = static_cast<char*>(buffer);
-    data->memory.push_back(*buff);
+    data->clear();
 
-    return nmemb;
+    char* bytes = static_cast<char*>(contents);
+    for (size_t i = 0; i < realsize; i++)
+    {
+        data->push_back(bytes[i]);
+    }
+
+    return realsize;
 }
-
-enum class Method
-{
-    POST,
-    GET  // not supported !
-};
 
 struct Response
 {
@@ -50,7 +45,12 @@ struct Response
 class HttpClient
 {
 public:
-    HttpClient()
+    HttpClient() = delete;
+
+    HttpClient(const std::string& eui, const std::string& secret)
+        : postUrl_("https://signomix.com/api/i4t")
+        , eui_("eui=" + eui)
+        , secretKey_(secret)
     {
         curl_global_init(CURL_GLOBAL_ALL);
     }
@@ -60,110 +60,82 @@ public:
         curl_global_cleanup();
     }
 
-    void setUrl(const std::string& url)
-    {
-        url_ = url;
-    }
-
-    void setMethod(const Method& method)
-    {
-        method_ = method;
-    }
-
     template <typename ValueType>
     void addField(const std::string& fieldName, const ValueType& value)
     {
         fields_ += "&" + fieldName + "=" + std::to_string(value);
     }
 
-    void setEui(const std::string& eui)
+    void clearFields()
     {
-        eui_ = "eui=" + eui;
+        fields_.clear();
     }
 
-    void setSecret(const std::string& secret)
-    {
-        secretKey_ = secret;
-    }
-
-    Response send()
+    Response sendPost()
     {
         Response response{false, __DEFALUT_CODE, __DEFALUT_CODE, "", {}};
 
-        if (method_ != Method::POST)
+        if (fields_.empty())
         {
-            /* for this moment only POST method is supported! */
             response.error = true;
             response.curlCode = CURLE_COULDNT_CONNECT;
-            response.description = "Only POST method is supported!";
+            response.description = "Empty fileds! POST request has not been sent.";
             return response;
         }
 
-        if (not url_.empty()
-            and not fields_.empty()
-            and not secretKey_.empty()
-            and not eui_.empty())
+        if (curl_)
         {
-            if (curl_)
-            {
-                curl_ = curl_easy_init();
-                curl_easy_setopt(curl_, CURLOPT_URL, url_.c_str());
+            curl_ = curl_easy_init();
+            curl_easy_setopt(curl_, CURLOPT_URL, postUrl_.c_str());
 
-                struct curl_slist *headers = NULL;
-                std::string authMess{"Authorization: " + secretKey_};
+            struct curl_slist *headers = NULL;
+            std::string authMess{"Authorization: " + secretKey_};
 
-                headers = curl_slist_append(headers, authMess.c_str());
-                headers = curl_slist_append(headers, "Accept: application/x-www-form-urlencoded");
+            headers = curl_slist_append(headers, authMess.c_str());
+            headers = curl_slist_append(headers, "Accept: application/x-www-form-urlencoded");
 
-                curl_easy_setopt(curl_, CURLOPT_HTTPHEADER, headers);
+            curl_easy_setopt(curl_, CURLOPT_HTTPHEADER, headers);
 
-                std::string message = eui_ + fields_;
+            std::string message = eui_ + fields_;
 
-                curl_easy_setopt(curl_, CURLOPT_POST, 1);
-                curl_easy_setopt(curl_, CURLOPT_POSTFIELDS, message.c_str());
-                curl_easy_setopt(curl_, CURLOPT_POSTFIELDSIZE, static_cast<long>(message.size()));
+            curl_easy_setopt(curl_, CURLOPT_POST, 1);
+            curl_easy_setopt(curl_, CURLOPT_POSTFIELDS, message.c_str());
+            curl_easy_setopt(curl_, CURLOPT_POSTFIELDSIZE, static_cast<long>(message.size()));
 
-                // ***** DATA TO CALLBACK ********
-                curl_easy_setopt(curl_, CURLOPT_WRITEDATA, &response.data);
-                curl_easy_setopt(curl_, CURLOPT_WRITEFUNCTION, writeCallback);
+            // ***** DATA TO CALLBACK ********
+            curl_easy_setopt(curl_, CURLOPT_WRITEDATA, &response.data);
+            curl_easy_setopt(curl_, CURLOPT_WRITEFUNCTION, writeCallback);
 
-                // ***** DATA TO FILE ********
-                // FILE * file;
-                // file = fopen("file_data.txt","w");
-                // curl_easy_setopt(curl_, CURLOPT_WRITEDATA, file);
+            // ***** DATA TO FILE ********
+            // FILE * file;
+            // file = fopen("response.txt","w");
+            // curl_easy_setopt(curl_, CURLOPT_WRITEDATA, file);
 
-                curlCode_ = curl_easy_perform(curl_);
+            curlCode_ = curl_easy_perform(curl_);
 
-                // getting HTTP code from response
-                curl_easy_getinfo(curl_, CURLINFO_RESPONSE_CODE, &response.httpCode);
+            // getting HTTP code from response
+            curl_easy_getinfo(curl_, CURLINFO_RESPONSE_CODE, &response.httpCode);
 
-                if(curlCode_ != CURLE_OK or response.httpCode != HTTP_CREATED)
-                {
-                    response.error = true;
-                    response.curlCode = curlCode_;
-                }
-                response.description = curl_easy_strerror(curlCode_);
-            
-                if (response.error and response.description == CURL_NO_ERROR)
-                {
-                    response.description = "HTTP error " + std::to_string(response.httpCode);
-                }
-            
-                curl_easy_cleanup(curl_);
-            }
-            else
+            if(curlCode_ != CURLE_OK or response.httpCode != HTTP_CREATED)
             {
                 response.error = true;
-                response.curlCode = CURLE_COULDNT_CONNECT;
-                response.description = "No connection!";
+                response.curlCode = curlCode_;
             }
+            response.description = curl_easy_strerror(curlCode_);
+        
+            if (response.error and response.description == CURL_NO_ERROR)
+            {
+                response.description = "HTTP error " + std::to_string(response.httpCode);
+            }
+
+            curl_easy_cleanup(curl_);
+            clearFields();
         }
         else
         {
             response.error = true;
             response.curlCode = CURLE_COULDNT_CONNECT;
-            response.description = "Make sure that you set method, eui, secret and fields. " \
-                "These data are necessarily for push a request!";
+            response.description = "No connection!";
         }
 
         return response;
@@ -173,11 +145,10 @@ private:
     CURL *curl_;
     CURLcode curlCode_;
 
+    std::string postUrl_;
     std::string eui_;
     std::string secretKey_;
-    std::string url_;
     std::string fields_;
-    Method method_;
 };
 
 } // namespace signomix
